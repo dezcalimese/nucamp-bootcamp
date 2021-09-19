a7a413d6abc698a1a321c2b5ad0c7d33f0a541ac
dezcalimese <75593051+dezcalimese@users.noreply.github.com>
2021-09-18 21:47:03 -0500

    Add files via upload

---
+++ 1-Fundamentals/weeks1-2/comparison_operators.py
@@ -0,0 +1,12 @@
+"""
+Python Comparison Operators
+"""
+
+X = 100
+Y = 200
+
+print(X != Y)				# X is not equal to Y
+print(X > Y)				# X is greater than Y
+print(X < Y) 				# X is less than Y
+print(X >= Y)				# X is greater than or equal to Y
+print(X <= Y)				# X is less than or equal to Y
---
+++ 1-Fundamentals/weeks1-2/composite_data_types.py
@@ -0,0 +1,17 @@
+"""
+Composite Data Types
+"""
+
+# Storing a List
+nucamp_locations = ["Seattle", "Tacoma", "Bellevue"]
+# Storing a Dictionary
+Bob_Info = {"name": "Bob", "age": 35, "cash": 100.25, "retired": False}
+# Storing a Tuple
+my_tuple = ("apple", "banana", "cherry")
+# Storing a Set
+my_set = {"cats", "dogs", "birds"}
+
+print("Data Type of the variable 'nucamp_location' is", type(nucamp_locations))
+print("Data Type of the variable 'Bob' is", type(Bob_Info))
+print("Data Type of the variable 'my_tuple' is", type(my_tuple))
+print("Data Type of the variable 'my_set' is", type(my_set))
